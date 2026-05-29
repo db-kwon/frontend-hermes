b9813ab70863e6b0abd64f06140f5fdf14d558ec
danbi kwon <dbkwon@babitalk.com>
2026-05-29 17:18:41 +0900

    feat(core): expose 6 retriever tools with JSON schema

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export { buildIndex, writeIndex } from "./indexer/index.js";
 export { buildFileGraph } from "./indexer/fileGraph.js";
 export { Retriever, loadIndex } from "./retriever/index.js";
 export * from "./types/index.js";
+export { TOOL_DEFINITIONS, executeTool } from "./tools/index.js";
